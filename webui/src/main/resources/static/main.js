c36e8b18d2f15bdc52363182bc8800de6ae3674a
evink01 <evink01@student.cs.ubc.ca>
2025-07-30 20:30:30 -0600

    Using the data from java to draw the GPX file as a polyline

--- webui/src/main/resources/static/main.js
+++ webui/src/main/resources/static/main.js
@@ -4,11 +4,30 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 // Fetch coordinate data from a track in the data folder
+
 let file = "13746599585.gpx";
 
 const url = `/api/track?file=${file}`;
 
 fetch(url)
   .then((response) => response.text())
-  .then((data) => console.log(data))
+  .then((data) => processJsonResponse(JSON.parse(data)))
   .catch((err) => console.error(err));
+
+function processJsonResponse(jsonData) {
+  let listOfCoordinates = jsonData.coordinates;
+  let polylineArray = [];
+
+  for (let i = 0; i < listOfCoordinates.length; i++) {
+    const coordinate = listOfCoordinates[i];
+    polylineArray.push([coordinate.lat, coordinate.lon]);
+  }
+  L.polyline(polylineArray, {
+    color: "blue",
+    weight: 5,
+  }).addTo(map);
+}
+
+function toCoordinateArray(coordinate) {
+  return [coordinate.lat, coordinate.lon];
+}
