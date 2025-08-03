15821de7e179eb0938999c2b8bb1d03f103ca1b2
evink01 <evink01@student.cs.ubc.ca>
2025-08-02 22:42:47 -0600

    made some custom icons for the markers in paint

--- webui/src/main/resources/static/main.js
+++ webui/src/main/resources/static/main.js
@@ -1,10 +1,13 @@
+// Set up map
 const map = L.map("map").setView([49.25, -123.11], 13);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "© OpenStreetMap contributors",
 }).addTo(map);
 
-// Fetch coordinate data from a track in the data folder
+// Event listeners
+map.on("click", setMarker);
 
+// Fetch coordinate data from a track in the data folder
 let file = "13746599585.gpx";
 
 const url = `/api/track?file=${file}`;
@@ -14,20 +17,22 @@ fetch(url)
   .then((data) => processJsonResponse(JSON.parse(data)))
   .catch((err) => console.error(err));
 
+// EFFECTS: Takes GPX json data and draws a line on the map
 function processJsonResponse(jsonData) {
   let listOfCoordinates = jsonData.coordinates;
   let polylineArray = [];
 
   for (let i = 0; i < listOfCoordinates.length; i++) {
-    const coordinate = listOfCoordinates[i];
-    polylineArray.push([coordinate.lat, coordinate.lon]);
+    polylineArray.push(listOfCoordinates[i]);
   }
+
   L.polyline(polylineArray, {
-    color: "blue",
+    color: "red",
     weight: 5,
   }).addTo(map);
 }
 
-function toCoordinateArray(coordinate) {
-  return [coordinate.lat, coordinate.lon];
+// EFFECTS: If no markers set, sets a start marker, otherwise sets end marker
+function setMarker(e) {
+  L.marker(e.latlng).addTo(map);
 }
